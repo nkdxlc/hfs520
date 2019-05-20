280ce07b5063cca2a740f2714c32bd255b367d69
nkdxlc <623990373@qq.com>
2019-05-20 21:04:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
-window.onload = function() {
-  document.getElementById('main-content').innerHTML = 'Hello, my princess  :) '
+const mainwrap = document.querySelector(".main-wrap");
+const openSearch = document.querySelector("#search-icon");
+const closeSearch = document.querySelector("#search-close");
+const formContainer = document.querySelector("#main-search-form");
+const searchContainer = document.querySelector("#search-container")
+const searchInput = document.querySelector(".search-input");
+const welcomeMessage = document.querySelector("#welcome");
+
+
+
+function init()
+{
+  welcomeMessage.classList.add("show-search");
+  initEvents();
 }
+
+function open()
+{
+  mainwrap.classList.add("move");
+  formContainer.classList.add("show-search");
+  searchContainer.classList.remove("opacity-off");
+  searchInput.focus();
+}
+
+function close()
+{
+  mainwrap.classList.remove("move");
+  formContainer.classList.remove("show-search");
+  searchContainer.classList.add("opacity-off");
+}
+
+function initEvents()
+{
+  openSearch.addEventListener('click', open);
+  closeSearch.addEventListener('click', close);
+  document.addEventListener('keyup', function(ev) {
+			if( ev.keyCode == 27 ) {
+				close();
+			}
+	});
+}
+
+
+init();
