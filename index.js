00fc922e92b47c438d11f50e112c11323fad3181
nkdxlc <623990373@qq.com>
2019-05-20 14:44:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 window.onload = function() {
-  document.getElementById('main-content').innerHTML = 'Hello, my princess :)'
+  document.getElementById('main-content').innerHTML = 'Hello, my princess. This is LC  :)'
 }
